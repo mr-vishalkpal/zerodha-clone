c4c61688a10aff85e46d3af2744de5781bcc191f
Vishal Kumar Pal <mr.vishalkpal@gmail.com>
2026-03-22 16:02:31 +0530

    Home UI update

---
+++ src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Hero from './Hero';
+import Brokerage from './Brokerage';
+
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+
+function PricingPage() {
+    return ( 
+        <>
+           
+            <Hero/>
+            <Brokerage/>
+          
+        </>
+     );
+}
+
+export default PricingPage;
